40d3e622af95aec3aa703e156a74e31b024f111c
smarteck4 <smarteck4@gmail.com>
2026-01-15 22:48:42 +0500

    update btn color

--- src/pages/Players.tsx
+++ src/pages/Players.tsx
@@ -107,7 +107,7 @@ export default function Players() {
                 />
                 {searchQuery && (
                   <Button
-                    variant="ghost"
+                    variant="destructive"
                     size="icon"
                     className="absolute right-1 top-1/2 -translate-y-1/2 h-7 w-7"
                     onClick={() => setSearchQuery('')}
